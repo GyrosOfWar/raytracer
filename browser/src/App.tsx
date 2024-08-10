a1cc92c8621509969b5488aea65a87c7c17defa2
Martin Tomasi <martin.tomasi@gmail.com>
2024-08-10 08:43:29 +0200

    hmmmm

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,5 +1,19 @@
+import init, { add } from "raytracer";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <p>Hello!</p>;
+  const [result, setResult] = useState<number | null>(null);
+  useEffect(() => {
+    init().then(() => {
+      setResult(add(16, 12));
+    });
+  });
+
+  return (
+    <div>
+      <p>Result: {result}</p>
+    </div>
+  );
 }
 
 export default App;
