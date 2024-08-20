20577440a55ebc77fedf0bef9b88efacfabc4548
Martin Tomasi <martin.tomasi@gmail.com>
2024-08-20 07:31:47 +0200

    render blackbody spectrum

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use image::{Pixel, Rgba};
 use mimalloc::MiMalloc;
 use pixels::wgpu::Color;
 use pixels::{Pixels, SurfaceTexture};
@@ -8,8 +9,10 @@ use raytracer::color::colorspace::{RgbColorSpace, S_RGB};
 use raytracer::color::rgb::Rgb;
 use raytracer::math::lerp;
 use raytracer::random::random;
+use raytracer::sample::stratified_1d;
 use raytracer::spectrum::{
-    Constant, HasWavelength, RgbAlbedo, SampledWavelengths, Spectrum, LAMBDA_MAX, LAMBDA_MIN,
+    Blackbody, Constant, HasWavelength, RgbAlbedo, SampledWavelengths, Spectrum, LAMBDA_MAX,
+    LAMBDA_MIN,
 };
 use tracing::{error, info, Level};
 use tracing_subscriber::fmt::format::FmtSpan;
@@ -61,34 +64,35 @@ fn color_to_slice(color: Rgb) -> [u8; 4] {
     [r, g, b, 255]
 }
 
-fn remap_range(value: f32, old_min: f32, old_max: f32, new_min: f32, new_max: f32) -> f32 {
-    (value - old_min) / (old_max - old_min) * (new_max - new_min) + new_min
-}
+fn create_spectrum_image(w: u32, h: u32) -> Vec<u8> {
+    use image::RgbaImage;
 
-fn render_spectrum(color_space: &RgbColorSpace, buffer: &mut [u8]) {
-    let start = std::time::Instant::now();
-    for (i, pixel) in buffer.chunks_exact_mut(4).enumerate() {
-        let x = i / WIDTH as usize;
-        let y = i % WIDTH as usize;
-        let lambda = remap_range(x as f32, 0.0, WIDTH as f32, LAMBDA_MIN, LAMBDA_MAX);
+    let cs = &S_RGB;
 
-        let spectrum = Spectrum::Constant(Constant { c: lambda });
+    let mut image = RgbaImage::new(w, h);
+    for (x, _, pixel) in image.enumerate_pixels_mut() {
+        let x_f = x as f32 / w as f32;
+        let temp = lerp(x_f, 1500.0, 9000.0);
+        let spectrum = Blackbody::new(temp);
         let mut color = Rgb::ZERO;
-        let n = 10;
-        for _ in 0..n {
+        for _ in 0..100 {
             let u = random();
-            let lambda = SampledWavelengths::sample_uniform(u);
-            let sample = spectrum.sample(&lambda);
-            let rgb = sample.to_rgb(lambda, color_space);
-            color += rgb;
+            let wavelengths = SampledWavelengths::sample_visible(u);
+            let sample = spectrum.sample(&wavelengths);
+            color += sample.to_rgb(wavelengths, cs);
         }
-        let color = color / n as f32;
 
-        pixel.copy_from_slice(&color_to_slice(color));
-        info!("pixel result: {:?}, color = {:?}", pixel, color);
+        color = color / 100.0;
+
+        pixel.0 = [
+            (color.r * 255.0).round() as u8,
+            (color.g * 255.0).round() as u8,
+            (color.b * 255.0).round() as u8,
+            255,
+        ];
     }
-    let elapsed = start.elapsed();
-    info!("rendered frame in {:?}", elapsed);
+
+    image.into_vec()
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -123,13 +127,13 @@ fn main() -> color_eyre::Result<()> {
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
 
-    let color_space = &S_RGB;
+    let image = create_spectrum_image(WIDTH, HEIGHT);
 
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
             let frame = pixels.frame_mut();
-            render_spectrum(color_space, frame);
+            frame.copy_from_slice(&image);
 
             if let Err(err) = pixels.render() {
                 error!("failed to render frame: {err}");
