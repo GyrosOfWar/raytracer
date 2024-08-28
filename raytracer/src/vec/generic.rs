9897964efb752db828f3aaee3dfa0d02df49068b
Martin Tomasi <martin.tomasi@gmail.com>
2024-08-28 18:23:39 +0200

    more generics

--- raytracer/src/vec/generic.rs
+++ raytracer/src/vec/generic.rs
@@ -1,9 +1,8 @@
-use std::ops::Mul;
+use std::ops::{Add, Mul};
 
-use num_traits::Float;
+use num_traits::{Float, One, Zero};
 
 use crate::bounds::Interval;
-use crate::impl_generic_binary_op;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vec3<T> {
@@ -70,40 +69,97 @@ where
     }
 }
 
-impl_generic_binary_op!(Add : add => (lhs: Vec3<T>, rhs: Vec3<T>) -> Vec3<T> {
-    Vec3::new(
-        lhs.x + rhs.x,
-        lhs.y + rhs.y,
-        lhs.z + rhs.z,
-    )
-});
+impl<T> One for Vec3<T>
+where
+    T: One + Copy,
+{
+    fn one() -> Self {
+        Vec3::all(T::one())
+    }
+}
 
-impl_generic_binary_op!(Mul : mul => (lhs: Vec3<T>, rhs: Vec3<T>) -> Vec3<T> {
-    Vec3::new(
-        lhs.x * rhs.x,
-        lhs.y * rhs.y,
-        lhs.z * rhs.z,
-    )
-});
+impl<T> Zero for Vec3<T>
+where
+    T: Zero + Copy,
+{
+    fn zero() -> Self {
+        Vec3::all(T::zero())
+    }
+
+    fn is_zero(&self) -> bool {
+        self.x.is_zero() && self.y.is_zero() && self.z.is_zero()
+    }
+}
 
 impl<T> Mul<T> for Vec3<T>
 where
-    T: Copy + Float,
+    T: Copy + Mul<Output = T>,
 {
-    type Output = Self;
+    type Output = Vec3<T>;
 
-    fn mul(self, rhs: T) -> Self {
+    fn mul(self, rhs: T) -> Vec3<T> {
         Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
     }
 }
 
-pub type Vec3f = Vec3<f32>;
+impl<T> Add for Vec3<T>
+where
+    T: Copy + Add<Output = T>,
+{
+    type Output = Self;
 
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl<T> Mul for Vec3<T>
+where
+    T: Copy + Mul<Output = T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
+    }
+}
+
+impl<T> Mul<T> for &Vec3<T>
+where
+    T: Copy + Mul<Output = T>,
+{
+    type Output = Vec3<T>;
+
+    fn mul(self, rhs: T) -> Vec3<T> {
+        Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+#[derive(Debug)]
 pub struct Point3<T> {
     pub x: T,
     pub y: T,
     pub z: T,
 }
 
+impl<T> Point3<T> {
+    fn new(x: T, y: T, z: T) -> Self {
+        Self { x, y, z }
+    }
+}
+
+impl<T> From<Vec3<T>> for Point3<T> {
+    fn from(v: Vec3<T>) -> Self {
+        Point3::new(v.x, v.y, v.z)
+    }
+}
+
+impl<T> From<Point3<T>> for Vec3<T> {
+    fn from(value: Point3<T>) -> Self {
+        Vec3::new(value.x, value.y, value.z)
+    }
+}
+
+pub type Vec3f = Vec3<f32>;
 pub type Point3f = Point3<f32>;
 pub type Point3fi = Point3<Interval>;
